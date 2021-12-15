4a924aaa16887e9c251dd4fe9317bc7fcef72664
Ndhossain <95048995+Ndhossain@users.noreply.github.com>
2021-12-16 01:54:30 +0600

    testimonial done

--- js files/style.js
+++ js files/style.js
@@ -14,3 +14,23 @@ cross.addEventListener('click', function slider() {
 })
 
 // testimonial
+
+let slide = document.getElementById('slider');
+let up = document.getElementById('upperArrow');
+let down = document.getElementById('downerArrow')
+
+let x = 0;
+
+up.addEventListener('click', function () {
+    if (x > '-40') {
+        x = x - 20;
+        slide.style.top = x + 'em';
+    }
+})
+
+down.addEventListener('click', function () {
+    if (x < 0) {
+        x = x + 20;
+        slide.style.top = x + 'em';
+    }
+})
